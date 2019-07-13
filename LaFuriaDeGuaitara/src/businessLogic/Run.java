9be0dabde48814f50fb9b12eeebb5f79276f3504
jdzapatao <jdzapatao@unal.edu.co>
2019-07-13 17:31:01 -0500

    Subiendo ventanas de inventario e instancia de elementos

--- LaFuriaDeGuaitara/src/businessLogic/Run.java
+++ LaFuriaDeGuaitara/src/businessLogic/Run.java
@@ -10,6 +10,7 @@ import GUI2.*;
 import data.*;
 import GUIsoundManagement.*;
 import java.awt.Graphics;
+import java.util.ArrayList;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 /**
@@ -28,6 +29,27 @@ public class Run {
         v1.add(panelInicio);
 
         v1.setVisible(true);*/
+        
+        
+        ItemPickeable item1 = new ItemPickeable("Abstracción", "Un topacio que crea "
+            + "versiones digitales de lo que sea que toque.", "Gema", 0, 0, "/src/Images/Sprites/item/1_topazSheet.ong");
+        ItemPickeable item2 = new ItemPickeable("Encapsulación", "Este rubí de sangre"
+            + " puede aislar a su portador del mundo real.", "Gema", 0, 0, "/src/Images/Sprites/item/2_rubySheet.png");
+        ItemPickeable item3 = new ItemPickeable("Herencia", "El safiro más preciado"
+            + " del mundo, puede obtener la habilidad de cualquiera de sus gemas "
+            + "gemelas.", "Gema", 0, 0, "/src/Images/Sprites/item/3_saphSheet.png");
+        ItemPickeable item4 = new ItemPickeable("Polimorfismo", "El poder comportarse"
+            + " de una manera dependiendo de su ambiente es la gran habilidad"
+            + " de esta esmeralda", "Gema", 0, 0, "/src/Images/Sprites/item/4_emeraldSheet.png");
+    
+        ArrayList<ItemPickeable> invent = new ArrayList<>();
+        invent.add(item1);
+        invent.add(item2);
+        invent.add(item3);
+        invent.add(item4);
+        
+        PanelInventario.setInv(invent);
+        
         Window v1 = new Window();
         v1.setVisible(true);
     }
